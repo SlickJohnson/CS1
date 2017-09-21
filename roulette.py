1fceee0ece6059fdaf3585f6195f34457a829550
SlickJohnson <liwa.johnson@gmail.com>
2017-09-20 18:56:59 -0700

    Update roulette game

--- roulette.py
+++ roulette.py
@@ -1,4 +1,5 @@
 import random
+
 random.seed(1)
 
 bank_account = 1000
@@ -20,39 +21,24 @@ def take_bet(color, number, amount):
 
 
 def roll_ball():
-    '''returns a random number between 0 and 37'''
-
     return random.randint(0, 38)
 
 
 def check_results(ball_roll, bet_info):
-    '''Compares bet_color to color rolled.  Compares
-    bet_number to number_rolled.'''
-
     print("The ball number is " + str(bet_info[2]) + "...\n")
     payout(ball_roll == bet_info[2], bet_info)
 
 
 def payout(did_win, bet_info):
-    '''returns total amount won or lost by user based on results of roll. '''
-
     if did_win:
         print("YOU WON! Your now have $", str(bank_account + bet_info[2]))
 
     else:
-        print("YOU LOST!!! HAHAHA! You now have $", str(bank_account - bet_info[2]))
+        print("YOU LOST!!! HAHAHA! You now have $",
+              str(bank_account - bet_info[2]))
 
 
 def play_game():
-    """This is the main function for the game.
-    When this function is called, one full iteration of roulette,
-    including:
-
-    Take the user's bet.
-    Roll the ball.
-    Determine if the user won or lost.
-    Pay or deduct money from the user accordingly.
-    """
     color = input("Choose color\n")
     number = 0
 
